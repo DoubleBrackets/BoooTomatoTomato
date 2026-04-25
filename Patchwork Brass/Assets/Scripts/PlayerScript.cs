22a37176dfe12c7eb966718969f239b02354b955
Arthur G <arthurgan29@gmail.com>
2026-04-25 01:18:55 -0700

    Installed unitask, primetween, purrnet packages, parrelsync, minimal networking setup

---
+++ Patchwork Brass/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,47 @@
+using System;
+using PurrNet;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerScript : NetworkBehaviour
+{
+    [SerializeField]
+    private InputAction _moveAction;
+
+    [SerializeField]
+    private float _moveSpeed;
+    
+    [SerializeField]
+    private Transform _targetTransform;
+
+    private Vector2 _moveDirection;
+    
+    void OnEnable()
+    {
+        _moveAction.Enable();
+        _moveAction.performed += OnMovePerformed;
+        _moveAction.canceled += OnMovePerformed;
+    }
+    
+    void OnDisable()
+    {
+        _moveAction.performed -= OnMovePerformed;
+        _moveAction.canceled -= OnMovePerformed;
+        _moveAction.Disable();
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext ctx)
+    {
+        _moveDirection = ctx.ReadValue<Vector2>();
+    }
+
+    public void Update()
+    {
+        if (!isOwner)
+        {
+            return;
+        }
+        
+        _targetTransform.position += new Vector3(_moveDirection.x, _moveDirection.y, 0).normalized * Time.deltaTime;
+    }
+}
