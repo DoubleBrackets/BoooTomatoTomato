8890e312a9bb04c7ea5b2f0324dfa8dac5ec99f2
endawghae <94265058+endawghae@users.noreply.github.com>
2026-04-26 03:56:36 -0700

    tomatoes

--- Tomato_Unity/Assets/Scripts/Gameplay/BullyController.cs
+++ Tomato_Unity/Assets/Scripts/Gameplay/BullyController.cs
@@ -2,36 +2,29 @@ using FishNet.Object;
 using Gameplay.Throwables;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 
 public class BullyController : NetworkBehaviour
 {
     [SerializeField]
-    InputActionMap _input;
-
-    [SerializeField]
     GameObject throwable;
 
-    /*
-    private void Awake()
+    public void OnAttack(CallbackContext ctx)
     {
-        _input = GetComponent<PlayerInput>().currentActionMap;
-    }
-
-    private void Update()
-    {
-        if (_input["Attack"].triggered)
-        {
-            Debug.Log("Spawn");
-            SpawnThrowable();
-        }
+        Debug.Log("owner check");
+        if (!IsOwner) return;
+        Debug.Log("attack");
+        if (!ctx.performed) return;
+        SpawnThrowable();
+        Debug.Log("attacked");
     }
 
     [ServerRpc]
     private void SpawnThrowable()
     {
+        Debug.Log("spawn tomato");
         GameObject obj = Instantiate(throwable, transform.position, Quaternion.identity);
         obj.GetComponent<Rigidbody>().linearVelocity = (transform.forward * 5) + (transform.up * 10);
         Spawn(obj);
     }
-    */
 }
