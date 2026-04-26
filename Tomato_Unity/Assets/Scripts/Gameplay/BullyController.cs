62a0749960d27e929b6a0517348e296f12eed4e7
endawghae <94265058+endawghae@users.noreply.github.com>
2026-04-26 02:58:23 -0700

    sanity check

--- Tomato_Unity/Assets/Scripts/Gameplay/BullyController.cs
+++ Tomato_Unity/Assets/Scripts/Gameplay/BullyController.cs
@@ -1,16 +1,37 @@
+using FishNet.Object;
+using Gameplay.Throwables;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class BullyController : MonoBehaviour
+public class BullyController : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    InputActionMap _input;
+
+    [SerializeField]
+    GameObject throwable;
+
+    /*
+    private void Awake()
     {
-        
+        _input = GetComponent<PlayerInput>().currentActionMap;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_input["Attack"].triggered)
+        {
+            Debug.Log("Spawn");
+            SpawnThrowable();
+        }
     }
+
+    [ServerRpc]
+    private void SpawnThrowable()
+    {
+        GameObject obj = Instantiate(throwable, transform.position, Quaternion.identity);
+        obj.GetComponent<Rigidbody>().linearVelocity = (transform.forward * 5) + (transform.up * 10);
+        Spawn(obj);
+    }
+    */
 }
